8caee8e52c6efa3559be84d55680614869ec135f
MansatCode <69149749+mansatCode@users.noreply.github.com>
2023-02-21 16:33:41 +0000

    Line connecting locations

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
-import MapView, { Marker } from 'react-native-maps'
+import MapView, { Marker, Polyline } from 'react-native-maps'
 
 const Map = () => {
   const BATH_INITIAL_REGION = {
@@ -14,23 +14,27 @@ const Map = () => {
     {
       id: 1,
       latitude: 51.382481020706635,
-      longitude: -2.3812105369639043
+      longitude: -2.3812105369639043,
+      pinColor:"orange",
     },
     { 
       id: 2,
       latitude: 51.38539845372343, 
-      longitude: -2.376339037524514
+      longitude: -2.376339037524514,
+      pinColor:"orange",
     },
     {
       id: 3,
       latitude: 51.37685566810041, 
-      longitude: -2.370529450651123
+      longitude: -2.370529450651123,
+      pinColor:"orange",
     }
   ]
 
   const markerElements = DUMMY_LOCATIONS.map((location) => (
     <Marker 
       coordinate={{longitude: location.longitude, latitude: location.latitude}}
+      pinColor={location.pinColor}
     />
   ))
 
@@ -44,8 +48,17 @@ const Map = () => {
       //onRegionChangeComplete -> runs when user stops dragging MapView
       onRegionChangeComplete={(region) => setRegion(region)}
     >
-      {/* Pass markers as direct children of MapView component */}
       {markerElements}
+      <Polyline 
+        coordinates={[
+          {longitude: DUMMY_LOCATIONS[0].longitude, latitude: DUMMY_LOCATIONS[0].latitude}, 
+          {longitude: DUMMY_LOCATIONS[1].longitude, latitude: DUMMY_LOCATIONS[1].latitude},
+          {longitude: DUMMY_LOCATIONS[2].longitude, latitude: DUMMY_LOCATIONS[2].latitude},
+        ]}
+        strokeColor={"#000"}
+        strokeWidth={3}
+        lineDashPattern={[1]}
+      />
     </MapView>
     <Text style={styles.text}>Current latitude: {region.latitude}</Text>
     <Text style={styles.text}>Current longitude: {region.longitude}</Text>
